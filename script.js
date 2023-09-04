f2ad8a6818d3e9c6bb03ea91ca1a75c7e3568904
wasp <margi4@op.pl>
2023-09-04 16:25:18 +0200

    clean up JS

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ function calcCulmulativeInterest() {
   let interestRate = Number(document.querySelector("#interest-rate").value);
   let timeUnit = Number(document.querySelector("#duration").value);
   let time = Number(document.querySelector("#time").value);
-  let totalAmount = investment * interestRate * time * timeUnit;
-  let totalInterest = investment + totalAmount;
+  let totalAmount = calculateTotalAmount(
+    investment,
+    interestRate,
+    time,
+    timeUnit
+  );
+  let totalInterest = calculateTotalInterest(investment, totalAmount);
+  displayResults(totalInterest, totalAmount);
+}
+function calculateTotalAmount(investment, interestRate, time, timeUnit) {
+  return investment * interestRate * time * timeUnit;
+}
+function calculateTotalInterest(investment, totalAmount) {
+  return investment + totalAmount;
+}
+function displayResults(totalInterest, totalAmount) {
   document.querySelector("#totalInterest").value = totalInterest;
   document.querySelector("#totalAmount").value = totalAmount;
 }
-
 document
   .querySelector("#calculate")
   .addEventListener("click", calcCulmulativeInterest);
